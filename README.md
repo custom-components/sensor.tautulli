8cefa4995800781120b0dd7b25d47c79d31cfe81
ludeeus <joasoe@gmail.com>
2018-07-20 09:30:51 +0200

    Added filter for keys and username

--- README.md
+++ README.md
@@ -12,6 +12,10 @@ sensor:
   platform: tautulli
   api_key: 24b6eac0a858748664878d146bf63623b4
   host: 192.168.1.14
+  username: username
+  keys:
+    - title
+    - media_type
 ```
 
 **Configuration variables:**  
@@ -20,6 +24,8 @@ key | description
 :--- | :---  
 **platform (Required)** | The sensor platform name.  
 **api_key (Required)** | Your Tautulli api_key  
-**host (Required)** | The IP adress of the server running Tautulli.  
+**host (Required)** | The IP adress of the server running Tautulli. 
+**username (Required)** | The Plex user you want to monitor. 
+**keys (Required)** | A list of [keys](keys.md) you want to monitor. 
 **port (Optional)** | The port the Tautulli uses, defaults to `8181`  
   
--- custom_components/sensor/tautulli.py
+++ custom_components/sensor/tautulli.py
@@ -5,53 +5,73 @@ For more details about this component, please refer to the documentation at
 https://github.com/custom-components/sensor.tautulli
 """
 import requests
+import logging
 import voluptuous as vol
 from homeassistant.helpers.entity import Entity
-from homeassistant.const import (CONF_API_KEY, CONF_HOST, CONF_PORT)
+from homeassistant.const import (CONF_API_KEY, CONF_HOST, CONF_PORT,CONF_USERNAME)
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.sensor import (PLATFORM_SCHEMA)
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
+_LOGGER = logging.getLogger(__name__)
+
+CONF_KEYS = 'keys'
 T_DATA = 'tautulli_data'
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_API_KEY): cv.string,
     vol.Required(CONF_HOST): cv.string,
+    vol.Required(CONF_USERNAME): cv.string,
     vol.Optional(CONF_PORT, default='8181'): cv.string,
-})
+    vol.Required(CONF_KEYS, default='None'):
+        vol.All(cv.ensure_list, [cv.string]),
+    })
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Create the sensor"""
     api_key = config.get(CONF_API_KEY)
     host = config.get(CONF_HOST)
     port = config.get(CONF_PORT)
-    add_devices([Tautulli(hass, api_key, host, port)])
+    keys = config.get(CONF_KEYS)
+    username = config.get(CONF_USERNAME)
+    add_devices([Tautulli(hass, api_key, host, port, keys, username)])
 
 class Tautulli(Entity):
     """Representation of a Sensor."""
 
-    def __init__(self, hass, api_key, host, port):
+    def __init__(self, hass, api_key, host, port, keys, username):
         """Initialize the sensor."""
         self.hass = hass
         self._api_key = api_key
         self._host = host
         self._port = port 
+        self._keys = keys
+        self._username = username
         self._state = None
         self.hass.data[T_DATA] = {}
         self.update()
 
     def update(self):
         """Method to update sensor value"""
-        url = 'http://' + self._host + ':' + self._port + '/api/v2?apikey=' + self._api_key + '&cmd=get_activity'
+        url = "http://{}:{}/api/v2?apikey={}&cmd=get_activity".format(self._host, self._port, self._api_key)
+        num = 0
         try:
             result = requests.get(url, timeout=5).json()
         except:
             self._state = "Not playing"
         try:
-            t_data = result['response']['data']['sessions'][0]
-            for attribute in t_data:
-                self.hass.data[T_DATA][str(attribute)] = str(t_data[attribute])
+            t_data = result['response']['data']['sessions']
+            for sessions in t_data:
+                if t_data[num]['username'].lower() == self._username.lower():
+                    t_data = t_data[num]
+                    for key in self._keys:
+                        try:
+                            self.hass.data[T_DATA][str(key)] = str(t_data[key])
+                        except:
+                            _LOGGER.debug('%s not found.', key)
+                else:
+                    num = num + 1
         except:
             self._state = "Not playing"
         try:
