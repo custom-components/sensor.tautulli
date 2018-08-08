dbf3e33235c7dfb5a8547c1f44b86ce0c14d08dd
ludeeus <joasoe@gmail.com>
2018-08-08 18:17:52 +0200

    varsion bump

--- custom_components/sensor/tautulli.py
+++ custom_components/sensor/tautulli.py
@@ -12,7 +12,7 @@ from homeassistant.const import (CONF_API_KEY, CONF_HOST, CONF_PORT)
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.sensor import (PLATFORM_SCHEMA)
 
-__version__ = '0.0.7'
+__version__ = '0.0.8'
 
 REQUIREMENTS = ['pytautulli==0.0.5']
 
